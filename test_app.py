6fb38a4eb98ea0c6ed10bfb8673e794d8adbbcec
jsabio <jasabio@my.cspc.edu.ph>
2026-04-25 20:52:07 +0800

    intentionally break test

--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
-    assert response.status_code == 200
+    assert b"Goodbye, World!" in response.data  
     assert b"Hello, World!" in response.data
